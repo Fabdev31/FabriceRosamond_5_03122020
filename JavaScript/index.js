ed3bab745ad56d32e19c9a99fe6259c79ce87f27
Fabrice ROSAMOND <fabrice.rosamond@hotmail.fr>
2021-06-25 01:00:10 +0200

    tentative d'integration des elements backs au frontend

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,10 +1,21 @@
-const myImage = document.querySelector('img');
-
-fetch('flowers.jpg')
-.then(function(response) {
-  return response.blob();
-})
-.then(function(myBlob) {
-  const objectURL = URL.createObjectURL(myBlob);
-  myImage.src = objectURL;
+const headers = new Headers({
+  "Content-type": "application/json"
 });
+
+fetch('http://localhost:3000/api/furniture')
+.then((response) => {
+  return response.json();
+})
+.then((furnitures) =>{
+  for(const furniture of furnitures){
+    console.log(furniture)
+  } 
+});
+
+function integrerCatalogueProduit(imageUrl, name, price, varnish){
+  const catalogueProduit = document.createElement("figure");
+  catalogueProduit.textContent = '<img>${imageUrl}<figcaption> ${name}</figcaption><h5>${price}</h5><p class="finition">${varnish}</p>';
+  document.querySelector("#figures").appendChild(figure)
+}
+
+integrerCatalogueProduit('../JWDP5/images/oak_1.jpg','Table bois design en chêne', 'prix', 'finition' );
