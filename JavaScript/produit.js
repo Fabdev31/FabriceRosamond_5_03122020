1bc1c8d095396b6573c766a2550bb503dab3b245
Fabrice ROSAMOND <fabrice.rosamond@hotmail.fr>
2021-07-07 10:50:36 +0200

    test implémentation automatique de l'affichage des options

--- JavaScript/produit.js
+++ JavaScript/produit.js
@@ -1,10 +1,13 @@
 const headers = new Headers({
   "Content-type": "application/json"
 });
-//
+//choisir dans les id 
 const params = window.location.search;
 const id = params.substring(params.indexOf('id=')+3, 28);
 
+
+
+//methode fetch avec la valeur de l'id à la fin de l'url
 fetch(`http://localhost:3000/api/furniture/${id}`)
 .then((response) => {
   return response.json();
@@ -18,11 +21,39 @@ fetch(`http://localhost:3000/api/furniture/${id}`)
     <div class="card-body" id="figures">
         <h5 class="Orimeubles text-center">Ori-ameublement</h5>
         <figure>
-          <figcaption>${furniture.name}</figcaption>
+          <figcaption>Nom du Produit : ${furniture.name}</figcaption>
           <a href="produit.html?id=${furniture._id}"><img src="${furniture.imageUrl}" 
           width="60" height="60" alt="photo de table en bois design"></a>
-          <h3>${furniture.price/1000} $</h3>
+          <h3>Prix : ${furniture.price/1000} $</h3>
+          <form>
+          <label for="types-de-vernis">
+              <select id="choix-vernissage">
+                <option value="choix">--choisir le type de vernis--</option>
+                <option value="" id="options-vernis"></option>
+              </select>
+          </label>
+          </form>
         </figure>
+        <button id="btn-ajouter-article">Ajouter au panier</button>
     </div>
-</div>`
+</div>`;
+//boucle for pour afficher les options
+const optionNombre=furniture.varnish;
+
+const select = document.getElementById("choix-vernissage");
+select.innerHTML =`<option value = "choix">--choisir le type de vernis--</option>`
+
+for(let i = 0; i < optionNombre.length; i++){
+  select.innerHTML +=`<option value =${i}>${optionNombre[i]}</option>`;
+  
+}
 })
+
+/*document
+  .getElementById("options-vernis")
+  .innerHTML+=
+  `<option value="${i}">${optionNombre[i]}</option>`; 
+}*/
+
+//travail sur les options
+
